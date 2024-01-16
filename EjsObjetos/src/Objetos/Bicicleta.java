c66efcf207f9a94b7495c4936634810e174ec659
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 09:00:30 +0100

    Ejercicio 3

---
+++ EjsObjetos/src/Objetos/Bicicleta.java
@@ -0,0 +1,15 @@
+package Objetos;
+
+public class Bicicleta {
+	// Atributos:
+		int numRuedas;
+		String tipo;
+		int tamaño;
+
+		// Método Constructor:
+		public Bicicleta() {
+			numRuedas = 2;
+			tipo = "Montaña";
+			tamaño = 26;
+}
+}
---
+++ EjsObjetos/src/Objetos/Uso_Bicicleta.java
@@ -0,0 +1,13 @@
+package Objetos;
+
+public class Uso_Bicicleta {
+
+	public static void main(String[] args) {
+		Bicicleta montaña = new Bicicleta();
+
+		System.out.println("La bicicleta es de  " + montaña.tipo
+				+ " su tamaño es de " + montaña.tamaño + ".");
+
+	}
+
+}
