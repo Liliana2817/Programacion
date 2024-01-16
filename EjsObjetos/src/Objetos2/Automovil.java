9a052af15c078e4aace20dd1a0ff7f9963b984de
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 13:12:56 +0100

    subir modificaciones de automovil y cuenta bancaria

--- EjsObjetos/src/Objetos2/Automovil.java
+++ EjsObjetos/src/Objetos2/Automovil.java
@@ -1,15 +1,17 @@
 package Objetos2;
 
 public class Automovil {
+	// Declarar las variables
   private String marca , modelo;
 
   
   public  Automovil (){
+	// construtor: Se define un constructor sin parámetros que establece un titular predeterminado
 	  marca = "citroen";
 	  modelo = "citroen c4";
   }
   
-  public void establece_marca(){
+  public void establece_marca(){//SETTER
 	  marca = "citroen";
   }
   public String dime_marca() {//GETTER
--- EjsObjetos/src/Objetos2/CuentaBancaria.java
+++ EjsObjetos/src/Objetos2/CuentaBancaria.java
@@ -1,18 +1,22 @@
 package Objetos2;
 
 public class CuentaBancaria {
+	// Declarar las variables
 	private String titular;
 	private double saldo;
 	
 public CuentaBancaria() {
+	// construtor: Se define un constructor sin parámetros que establece un titular predeterminado
 	titular="Lili";
 	saldo=5000;
 }
 	
-	public String dime_titular(){
+	public String dime_titular(){//GETTER
+		//metodo:  Devuelve una cadena que contiene información sobre el titular de la cuenta.
 		return " El titular de la cuenta es  " + titular + " .";
 	}
-	public void establece_titular(){
+	public void establece_titular(){//SETTER
+		// metodo: Establece el titular de la cuenta
 		titular = "Lili";
 	}
 	
@@ -21,6 +25,7 @@ public CuentaBancaria() {
 		
 	}
 	public void establece_saldo(){
+		/*Intenta establecer el saldo de la cuenta. Verifica si el saldo es no negativo antes de establecerlo. Si el saldo proporcionado es negativo, imprime un mensaje de advertencia.*/
 		if (saldo >= 0) {
 			saldo = saldo;
  
