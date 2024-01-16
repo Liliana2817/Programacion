4f96429ffd5ecaa35c302fc86bde417f6a306439
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 08:53:27 +0100

    Segundo Ejercicio

---
+++ EjsObjetos/src/Objetos/Libro.java
@@ -0,0 +1,15 @@
+package Objetos;
+
+public class Libro {
+	
+
+	String titulo;
+	String autor;
+	int numPaginas;
+
+	public Libro(){
+		titulo="Adios mundo cruel";
+		autor="La vida";
+		numPaginas=100;
+}
+}
---
+++ EjsObjetos/src/Objetos/Uso_Libro.java
@@ -0,0 +1,12 @@
+package Objetos;
+
+public class Uso_Libro {
+
+	public static void main(String[] args) {
+		// 
+		
+		Libro saga = new Libro();
+		System.out.println("Titulo del libro "+ saga.titulo + " y ha sido escrito por " + saga.autor);
+	}
+
+}
