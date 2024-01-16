73a74db544e84f144bd816abfef592a7139ff88c
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 09:06:04 +0100

    Ejercicio 4

---
+++ EjsObjetos/src/Objetos/Reloj.java
@@ -0,0 +1,16 @@
+package Objetos;
+
+public class Reloj {
+
+	// Atributos:
+		String marca;
+		boolean digital;
+		double precio;
+
+		// - Método Constructor:
+		public Reloj() {
+			marca = "lotus";
+			digital = true;
+			precio = 50.0;
+}
+}
---
+++ EjsObjetos/src/Objetos/Uso_Reloj.java
@@ -0,0 +1,12 @@
+package Objetos;
+
+public class Uso_Reloj {
+
+	public static void main(String[] args) {
+		Reloj lotus = new Reloj();
+
+		System.out.println(" El reloj es digital : " + lotus.digital  + " de la marca " + lotus.marca + " Cuesta " +  lotus.precio +  " €.");
+
+	}
+
+}
