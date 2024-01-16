2b61827141a681087067dde956833a5928a0689e
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 09:09:52 +0100

    Ejercicio 5

---
+++ EjsObjetos/src/Objetos/Celular.java
@@ -0,0 +1,13 @@
+package Objetos;
+
+public class Celular {
+	String modelo;
+	int almacenamiento;
+	int camaraMP;
+	
+	public Celular() {
+		modelo = "Samsung";
+		almacenamiento = 64;
+		camaraMP = 12;
+}
+}
---
+++ EjsObjetos/src/Objetos/Uso_Celular.java
@@ -0,0 +1,11 @@
+package Objetos;
+
+public class Uso_Celular {
+
+	public static void main(String[] args) {
+		Celular Samsung = new Celular();
+		System.out.println("El modelo del celular es " + Samsung.modelo+ " y su capacidad de almacenamiento es de " + Samsung.almacenamiento+ " GB");
+
+	}
+
+}
