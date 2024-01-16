06895a28abc61d5359df788a068a4da31c019f04
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-16 08:49:02 +0100

    Ejercicio 1  modificacion

--- EjsObjetos/src/Objetos/Uso_Lampara.java
+++ EjsObjetos/src/Objetos/Uso_Lampara.java
@@ -5,7 +5,7 @@ public class Uso_Lampara {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       Lampara LamparaTecho = new Lampara();
-      System.out.println("La Lampara es de color " +  LamparaTecho.color );
+      System.out.println("La Lampara es de color " +  LamparaTecho.color + " , tiene una potencia de " +  LamparaTecho.potencia + ", y de tipo " +  LamparaTecho.tipo);
 	}
 
 }
