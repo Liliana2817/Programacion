a56b13948783580a779a9a12304ecb48c07865dd
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-01-29 10:42:12 +0100

    Ejercicio 2: Modificación del Sueldo

---
+++ EjsObjetos/src/Objetos5/ModificacionSueldo.java
@@ -0,0 +1,18 @@
+package Objetos5;
+
+public class ModificacionSueldo {
+
+	public static void main(String[] args) {
+		 Empleado Empleado2 = new Empleado("Mari", 3000.0, 2, 2,0);
+         
+         //Muestra el sueldo actual
+       System.out.println("Su sueldo actual del empleado2 es de: " + Empleado2.dameSueldo() + "€");
+        
+         //Aumenta el sueldo del empleado
+         Empleado2.subeSueldo(10);
+	
+         //Nuevo sueldo del empleado
+      System.out.println("Nuevo sueldo del empleado2 es: " + Empleado2.dameSueldo() + "€");
+	}
+
+}
