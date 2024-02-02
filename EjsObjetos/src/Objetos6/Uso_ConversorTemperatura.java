4aa4d7da6c19a69ccac9a3ba323e3bd88c43cc9b
Liliana garcia severiche <garciasevericheliliana@gmail.com>
2024-02-02 11:04:48 +0100

    Modificado

--- EjsObjetos/src/Objetos6/Uso_ConversorTemperatura.java
+++ EjsObjetos/src/Objetos6/Uso_ConversorTemperatura.java
@@ -3,13 +3,13 @@ import javax.swing.JOptionPane;
 public class Uso_ConversorTemperatura {
 
 	public static void main(String[] args) {
-		// Obtener la temperatura en Celsius del usuario
+		// Obtener la temperatura en Celsius 
         String inputCelsius = JOptionPane.showInputDialog("Ingresa la tempratura en Celsius:");
 
         // Convertir la entrada a tipo double
         double temperaturaCelsius = Double.parseDouble(inputCelsius);
 
-        // Crear un objeto de la clase Conversor y llamar al método de conversión
+        // Crear un objeto de la clase ConversorTemperatura y llamar 
         ConversorTemperatura.convertirYMostrar(temperaturaCelsius);
 	}
 
